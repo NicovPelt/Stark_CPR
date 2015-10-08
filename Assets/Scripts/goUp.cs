31f6209a12e31d5654995b0983a23ef82bc322d8
HawkHunt <cobraluuk@hotmail.com>
2015-10-08 12:14:08 +0200

    Version 0.1
    1st commit includes: Cube, BPM, rotating visuals.

---
+++ Assets/Scripts/goUp.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class goUp : MonoBehaviour 
+{
+
+
+	public Vector3 forceUp = new Vector3();
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		//GameObject this;
+		Rigidbody rb = this.GetComponent<Rigidbody>();
+
+		if (Input.GetKeyDown ("space")) 
+		{
+			rb.AddForce (forceUp);
+		}
+
+		//float movehorizontal = Input.getAxis ("Horizontal") * playerSpeed;
+		//rb.AddForce (Vector3.up);
+	}
+}
---
+++ Assets/Scripts/goUpTransform.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class goUpTransform : MonoBehaviour {
+
+	public float bpm = 0.6f;
+	public float bpmMax = 0.4f;
+	public float bpmMin = 0.8f;
+	public Vector3 lowV;
+	public Vector3 highV;
+
+
+	float tempTime = 0.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		tempTime += Time.deltaTime;
+		if (Input.GetKeyDown ("space"))
+		{
+
+			if (tempTime >= bpmMin)
+			{
+				transform.position = lowV;
+				//dynamic cubeColor = gameObject.GetComponent<Shader>();
+
+
+			}
+			else if (tempTime <= bpmMax)
+			{
+				transform.position = highV;
+			}
+			else
+			{
+				transform.position = (new Vector3 (0.0f, 0.0f, 0.0f));
+			}
+			tempTime = 0.0f;
+		}
+
+
+	}
+}
