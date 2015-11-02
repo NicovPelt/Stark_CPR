06e9f79de9296c25404e7de2bebe2765b0f7152e
HawkHunt <cobraluuk@hotmail.com>
2015-11-02 15:20:33 +0100

    Updated Scene to Look nice

---
+++ Assets/Scripts/MovingSeaHorses.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingSeaHorses : MonoBehaviour {
+
+	float rotateSpeed =0.25f;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//gameObject.transform.forward (Vector3.forward * forwardSpeed);
+		gameObject.transform.Rotate (Vector3.up * -rotateSpeed);
+
+	}
+}
