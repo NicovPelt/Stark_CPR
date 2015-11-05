834edb02bd2e7ff3bbd4878a3003d46143478c09
HawkHunt <cobraluuk@hotmail.com>
2015-11-05 11:04:12 +0100

    life system implemented

--- Assets/Scripts/playerIsKill.cs
+++ Assets/Scripts/playerIsKill.cs
@@ -6,24 +6,47 @@ public class playerIsKill : MonoBehaviour {
 	public GameObject player;
 	int deadScore = 0; 
 
-	
+
+	public GameObject life1;
+	public GameObject life2;
+	public GameObject life3;
+	public GameObject dead1;
+	public GameObject dead2;
+
 	void Start () 
 	{
+//		Sprite life1 = Resources.Load <Sprite> ("biervis");
+//		Sprite life2 = Resources.Load <Sprite> ("biervis");
+//		Sprite life3 = Resources.Load <Sprite> ("biervis");
 
 	}
 
 	void OnTriggerEnter (Collider other)
 	{
-		if (deadScore < 3) 
+		deadScore++;
+
+		if (deadScore == 1) 
 		{
-			Debug.Log ("Dedded");
-			deadScore++;
+			dead1.SetActive(true);
+			Debug.Log ("dedded once");
+
 		}
-		if (deadScore == 3)
+		if (deadScore == 2) 
 		{
-		Destroy (player);
-		Application.LoadLevel ("Level1");
+			dead2.SetActive(true);
+			Debug.Log ("dedded twice");
+
+
 		}
+		if (deadScore == 3) 
+		{
+
+			Debug.Log ("dedded final");
+			Destroy (player);
+			Application.LoadLevel ("Level1");
+
+		}
+	
 
 	}
 
